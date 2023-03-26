a3e59befb045c006c8854d73575dbc128ed6c1b5
Callen Ward <wardcallen95@gmail.com>
2023-03-26 18:16:42 +0100

    add incremental selection and auto install to treesitter config

--- lua/plugin_config.lua
+++ lua/plugin_config.lua
@@ -1,13 +1,22 @@
 require('nvim-treesitter.configs').setup {
     ensure_installed = { 'lua', 'rust', 'typescript', 'javascript' },
-    auto_install = false,
+    auto_install = true,
 
     highlight = {
         enable = true
     },
     indent = {
         enable = true
-    }
+    },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "gnn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
+        },
+    },
 }
 
 require('lualine').setup {
