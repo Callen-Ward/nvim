9b97d33b107c0ce8e9f85c76460139626267f3f6
Callen Ward <wardcallen95@gmail.com>
2023-04-10 13:59:14 +0100

    change double quotes to single quotes in treesitter config

--- lua/plugin_config.lua
+++ lua/plugin_config.lua
@@ -3,7 +3,7 @@ require('nvim-treesitter.configs').setup {
     auto_install = true,
 
     highlight = {
-        enable = true
+        enable = true,
     },
     indent = {
         enable = true
@@ -11,12 +11,12 @@ require('nvim-treesitter.configs').setup {
     incremental_selection = {
         enable = true,
         keymaps = {
-            init_selection = "gnn",
-            node_incremental = "grn",
-            scope_incremental = "grc",
-            node_decremental = "grm",
+            init_selection = 'gnn',
+            node_incremental = 'grn',
+            scope_incremental = 'grc',
+            node_decremental = 'grm',
         },
-    },
+    }
 }
 
 require('lualine').setup {
