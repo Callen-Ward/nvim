45c84166ee267b11686f39812d8d81b24fa2e6f1
Callen Ward <wardcallen95@gmail.com>
2023-04-17 22:56:30 +0100

    refactor keybind in neovide mappings file

--- lua/neovide/mappings.lua
+++ lua/neovide/mappings.lua
@@ -4,15 +4,14 @@ local map_opts = { noremap = true, silent = true }
 vim.keymap.set('i', '<C-v>', '<Right><ESC>"+Pa', map_opts)
 
 -- increase neovide_scale_factor by 0.1
-local function scaleup()
-    local fscale = vim.g.neovide_scale_factor
-    if fscale then
-        vim.g.neovide_scale_factor = fscale + 0.1
+vim.keymap.set('n', '<C-=>', function()
+    local scalefactor = vim.g.neovide_scale_factor
+    if scalefactor then
+        vim.g.neovide_scale_factor = scalefactor + 0.1
     else
         vim.g.neovide_scale_factor = 1.1
     end
-end
-vim.keymap.set('n', '<C-=>', scaleup, map_opts)
+end, map_opts)
 
 -- reset scale factor
 vim.keymap.set('n', '<C-->', function() vim.g.neovide_scale_factor = 1 end, map_opts)
