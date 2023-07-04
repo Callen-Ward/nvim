14cc4dee50e51f97e63661adc1f6ac7ef11ab0f4
Callen Ward <wardcallen95@gmail.com>
2023-07-04 15:37:02 +0100

    update mappings file

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = ','
 
 local map_opts = { noremap = true, silent = true }
 
--- copy to clipboard clipboard
+-- copy to system clipboard
 vim.keymap.set('v', '<c-c>', '"+y', map_opts)
 
 -- use alt+j/k to move current line or selection
@@ -18,36 +18,22 @@ vim.keymap.set('n', 'k', 'gk', map_opts)
 -- disable search highlighting
 vim.keymap.set('n', '<leader>/', ':noh<CR>', map_opts)
 
--- terminal {{{
-
+-- terminal
 vim.keymap.set('t', '<C-n>', '<C-\\><C-n>', map_opts)
 vim.keymap.set('t', '<C-w>', '<C-\\><C-n><C-w>', map_opts)
-vim.keymap.set('t', '<C-h>', '<Cmd>wincmd h<CR>', map_opts)
-vim.keymap.set('t', '<C-j>', '<Cmd>wincmd j<CR>', map_opts)
-vim.keymap.set('t', '<C-k>', '<Cmd>wincmd k<CR>', map_opts)
-vim.keymap.set('t', '<C-l>', '<Cmd>wincmd l<CR>', map_opts)
 
--- }}}
-
--- diagnostics {{{
-
+-- diagnostics
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, map_opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, map_opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, map_opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, map_opts)
 
--- }}}
-
--- nvim-tree {{{
-
+-- nvim-tree
 vim.keymap.set('n', '<c-n>', ':NvimTreeToggle<CR>', map_opts)
 vim.keymap.set('n', '<leader>n', ':NvimTreeFocus<CR>', map_opts)
 vim.keymap.set('n', '<M-n>', ':NvimTreeFindFile<CR>', map_opts)
 
--- }}}
-
--- NERDCommenter {{{
-
+-- NERDCommenter
 -- set twice for compatibility
 vim.keymap.set('i', '<c-_>', '<ESC><PLUG>NERDCommenterTogglei', map_opts)
 vim.keymap.set('n', '<c-_>', '<PLUG>NERDCommenterToggle', map_opts)
@@ -57,23 +43,15 @@ vim.keymap.set('i', '<c-/>', '<ESC><PLUG>NERDCommenterTogglei', map_opts)
 vim.keymap.set('n', '<c-/>', '<PLUG>NERDCommenterToggle', map_opts)
 vim.keymap.set('v', '<c-/>', '<PLUG>NERDCommenterToggle', map_opts)
 
--- }}}
-
--- telescope {{{
-
+-- telescope
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<c-p>', builtin.find_files, map_opts)
 vim.keymap.set('n', '<leader>fb', builtin.buffers, map_opts)
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, map_opts)
 
--- }}}
-
--- vsnip {{{
-
-vim.cmd('inoremap <expr> <Tab>   vsnip#jumpable(1)   ? "<Plug>(vsnip-jump-next)"      : "<Tab>"')
-vim.cmd('snoremap <expr> <Tab>   vsnip#jumpable(1)   ? "<Plug>(vsnip-jump-next)"      : "<Tab>"')
-vim.cmd('inoremap <expr> <S-Tab> vsnip#jumpable(-1)  ? "<Plug>(vsnip-jump-prev)"      : "<S-Tab>"')
-vim.cmd('snoremap <expr> <S-Tab> vsnip#jumpable(-1)  ? "<Plug>(vsnip-jump-prev)"      : "<S-Tab>"')
-
--- }}}
+-- vsnip
+vim.cmd('inoremap <expr> <Tab>   vsnip#jumpable(1)  ? "<Plug>(vsnip-jump-next)" : "<Tab>"')
+vim.cmd('snoremap <expr> <Tab>   vsnip#jumpable(1)  ? "<Plug>(vsnip-jump-next)" : "<Tab>"')
+vim.cmd('inoremap <expr> <S-Tab> vsnip#jumpable(-1) ? "<Plug>(vsnip-jump-prev)" : "<S-Tab>"')
+vim.cmd('snoremap <expr> <S-Tab> vsnip#jumpable(-1) ? "<Plug>(vsnip-jump-prev)" : "<S-Tab>"')
