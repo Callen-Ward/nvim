c44b1f3597d79af855fb63866521e5d72e2b798f
Callen Ward <wardcallen95@gmail.com>
2023-07-05 17:25:39 +0100

    move neovide mappings from lua/neovide/mappings.lua to lua/mappings.lua

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -114,4 +114,23 @@ M.set_lsp_mappings = function(bufopts, format)
     vim.keymap.set('n', '<space>f', function() format() end, bufopts)
 end
 
+-- neovide
+M.set_neovide_mappings = function()
+    -- paste from clipboard
+    vim.keymap.set('i', '<C-v>', '<Right><ESC>"+Pa', map_opts)
+
+    -- increase neovide_scale_factor by 0.1
+    vim.keymap.set('n', '<C-=>', function()
+        local scalefactor = vim.g.neovide_scale_factor
+        if scalefactor then
+            vim.g.neovide_scale_factor = scalefactor + 0.1
+        else
+            vim.g.neovide_scale_factor = 1.1
+        end
+    end, map_opts)
+
+    -- reset scale factor
+    vim.keymap.set('n', '<C-->', function() vim.g.neovide_scale_factor = 1 end, map_opts)
+end
+
 return M
--- lua/neovide/neovide.lua
+++ lua/neovide/neovide.lua
@@ -15,4 +15,4 @@ vim.g.neovide_cursor_animation_length = 0.08
 vim.g.neovide_cursor_trail_size = 0.5
 vim.g.neovide_cursor_vfx_mode = ''
 
-require('neovide.mappings')
+require('mappings').set_neovide_mappings()
